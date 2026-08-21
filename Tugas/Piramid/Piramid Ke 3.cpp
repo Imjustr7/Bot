73f794a23889803ead831703e0d6bd620f7e1c6c
Imjustr7 <have84770@gmail.com>
2026-08-21 14:26:21 +0700

    Update Piramid Ke 3.cpp

--- Tugas/Piramid/Piramid Ke 3.cpp
+++ Tugas/Piramid/Piramid Ke 3.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 int main() {
     int rows = 5;
-
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= rows - i; j++) {
+    do {
+        int j = 1;
+        while (j <= rows) {
             cout << " ";
+            j++;
         }
-        for (int k = 1; k <= (2 * i - 1); k++) {
+        int k = 1;
+        while (k <= (5 - (rows - 1))) {
             cout << "*";
+            k++;
         }
-
         cout << endl;
-    }
-
+        rows--;
+    } while (rows > 0);
     return 0;
 }
